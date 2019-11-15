3003016a13bb34291bc59ffb308a49a984aeb575
dixoncs <dixoncs@appstate.edu>
2019-11-15 10:51:16 -0500

    working on Fibonacci

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -16,6 +16,80 @@ public class FibonacciHeap
     }
 
     /**
+     * Adds a new 1 element heap into the root-list.
+     */
+    public insert()
+    {
+        System.out.println("This is a method");
+    }
+
+    /**
+     * Links together the root-lists of the two heaps.
+     */
+    public merge()
+    {
+        System.out.println("This is a method");
+    }
+    
+    /**
+     * Joins equal-ranked trees in the root-list to ensure only one tree of 
+     * each rank remains.
+     */
+    public consolidate()
+    {
+        System.out.println("This is a method");
+    }
+
+    /**
+     * Removes the minimum ... from the tree.
+     * O(log n) time
+     */
+    public remove_min()
+    {
+        System.out.println("This is a method");
+    }
+ 
+    /**
+     *
+     */
+    public decrease_key()
+    {
+        System.out.println("This is a method");
+    }
+ 
+    /**
+     *
+     */
+    public delete()
+    {
+        System.out.println("This is a method");
+    }
+   
+    /**
+     *
+     */
+    public increase_key()
+    {
+        System.out.println("This is a method");
+    }   
+
+    /**
+     *
+     */
+    public sift_up()
+    {
+        System.out.println("This is a method");
+    }
+
+    /**
+     *
+     */
+    public sift_down()
+    {
+        System.out.println("This is a method");
+    }
+
+    /**
      * Main method.
      * @param args command-line arguments
      */
