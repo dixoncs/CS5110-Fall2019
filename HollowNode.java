7213d2e3a5b89ccb66eba6ea6a3a7bf612bf69cf
dixoncs <dixoncs@appstate.edu>
2019-11-29 20:43:57 -0500

    making hollow heap

---
+++ HollowNode.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class  HollowNode
+{
+    //private element;
+    //private key;
+    //private h;
+    
+    
+    /**
+     *
+     *
+     */
+    //public getKey()
+    //{
+    //  return key;
+    //} 
+
+}
