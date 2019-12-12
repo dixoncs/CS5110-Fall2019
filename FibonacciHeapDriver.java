e032952a3c9f1030ee5646ab7e39c43b1e3c6cba
dixoncs <46690479+dixoncs@users.noreply.github.com>
2019-12-12 10:49:42 -0500

    driver for Fibonacci Heaps

---
+++ FibonacciHeapDriver.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+
+/**
+ * Fibonacci Heap driver class.
+ * @author Courtney Dixon
+ * @version 11/9/2019
+ */
+public class FibonacciHeapDriver {
+    
+    /**
+     * Main method.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        FibonacciHeap testFibHeap = new FibonacciHeap();
+        testFibHeap.insert(new FibonacciNode());
+    }
+}
