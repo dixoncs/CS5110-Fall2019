ea18fb30887d2308204df397c642fb1926f81c6d
dixoncs <46690479+dixoncs@users.noreply.github.com>
2019-12-12 10:50:39 -0500

    driver for hollow heap

---
+++ HollowHeapDriver.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+
+/**
+ * Hollow Heap driver class.
+ * @author Courtney Dixon
+ * @version 11/9/2019
+ */
+public class HollowHeapDriver {
+    
+    /**
+     * Main method.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+       HollowHeap testHollowHeap = new HollowHeap();
+       testHollowHeap.insert(new HollowNode());
+    }
+}
