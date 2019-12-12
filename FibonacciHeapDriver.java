886ac91d0815fa88b666c288f556ca407775f1a6
dixoncs <dixoncs@appstate.edu>
2019-12-12 11:19:57 -0500

    updating everything

--- FibonacciHeapDriver.java
+++ FibonacciHeapDriver.java
@@ -59,12 +59,12 @@ public class FibonacciHeapDriver {
         System.out.println("-----------------------------------------------------\n");
         
         System.out.println("Running test 2");
-        FibonacciNode n1 = new FibonacciNode(6);
-        FibonacciNode n2 = new FibonacciNode(4);
-        FibonacciNode n3 = new FibonacciNode(8);
-        fh.insert(n1);
-        fh.insert(n2);
-        fh.insert(n3);
+        FibonacciNode n4 = new FibonacciNode(6);
+        FibonacciNode n5 = new FibonacciNode(4);
+        FibonacciNode n6 = new FibonacciNode(8);
+        fh.insert(n4);
+        fh.insert(n5);
+        fh.insert(n6);
 
         System.out.println("-----------------------------------------------------");
         System.out.println("Testing insert. Heap should contain 4,6,8");
@@ -72,8 +72,8 @@ public class FibonacciHeapDriver {
         System.out.println("-----------------------------------------------------\n");
 
         System.out.println("-----------------------------------------------------");
-        System.out.println("Decreasing key of n2 (4) to 3");
-        fh.decreaseKey(n2, 3);
+        System.out.println("Decreasing key of n4 (4) to 3");
+        fh.decreaseKey(n4, 3);
         fh.displayHeap();
         System.out.println("-----------------------------------------------------\n");
 
@@ -91,14 +91,14 @@ public class FibonacciHeapDriver {
 
         System.out.println("-----------------------------------------------------");
         System.out.println("Testing delete on 8. Heap should contain 6");
-        fh.delete(n3);
+        fh.delete(n6);
         System.out.println("The new min should be 6. Min is: " + fh.findMin().key);
         fh.displayHeap();
         System.out.println("-----------------------------------------------------\n");
 
         System.out.println("-----------------------------------------------------");
         System.out.println("Testing delete on 6. Heap should contain nothing.");
-        fh.delete(n2);
+        fh.delete(n5);
         System.out.println("The new min should be null. Min is: " + fh.findMin());
         fh.displayHeap();
         System.out.println("-----------------------------------------------------\n");
